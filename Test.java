d7e1fd31f64f7afa6a0af7136ff523339aec4aa4
Umar Solanki <uysolanki@gmail.com>
2024-09-10 20:55:45 +0530

    added Test file

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test
+{
+	public static void main(String x[])
+    {
+     System.out.println("Hello World");
+    }
+	
+	public static void main(int a)
+    {
+     System.out.println("Hello World");
+    }
+	
+	public static void main()
+    {
+     System.out.println("Hello World");
+    }
+}	
+
+
+if we wish to permit an outside program to "DIRECTLY ACCESS"
+a method then that method should be made public
